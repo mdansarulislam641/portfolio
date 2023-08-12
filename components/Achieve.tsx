98a53e0a896a288af739dae2fd5014da498b5f41
mdansarulislam641 <mdansarulislam641.com>
2023-08-12 10:04:47 +0600

    fully done achieve section

--- components/Achieve.tsx
+++ components/Achieve.tsx
@@ -15,11 +15,11 @@ const Achieve = () => {
         <section>
             <div className='w-full'>
                 {/* card  */}
-                <div className='grid grid-cols-1 lgl:grid-cols-2 gap-5'>
+                <div className='grid grid-cols-1 lgl:grid-cols-2 gap-10 px-5'>
                 <PhotoProvider>
                    { images.map(( img , idx) => (
                       <PhotoView key={idx}  src={img}> 
-                            <Image className='w-full h-[500px] object-fill' src={img} width={1000} height={500} alt='achieve-image'/>
+                            <Image className='w-full md:w-2/3 mx-auto lgl:w-full h-[500px] object-fill cursor-pointer rounded-lg shadow-cardShadow'  title='click to view' src={img} width={1000} height={500} alt='achieve-image' />
                       </PhotoView>
                    ))}
                  
