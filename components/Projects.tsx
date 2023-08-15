a0e8aca8bf635eb7a227b67f1b06e2307a029867
mdansarulislam641 <mdansarulislam641.com>
2023-08-15 11:38:15 +0600

    project part redesign

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,118 +1,78 @@
 import React from "react";
 import SectionTitle from "./SectionTitle";
 import Image from "next/image";
-import images from "../public/assets/images/profile.jpeg";
-import { TbBrandGithub } from "react-icons/tb";
+import { FaCircleInfo } from "react-icons/fa6";
+import { MdOutlineLiveTv } from "react-icons/md";
+import eventMart from '../public/assets/projectImage/event-Mart-min.png'
+import laptop from '../public/assets/projectImage/reseleLaptop-min.png'
+import onlineService from '../public/assets/projectImage/onlineService-min.png'
 const Projects = () => {
+  
   return (
-    <section id="projects" className="max-w-container mx-auto lgl:px-20 py-24">
+    <section id="projects" className="max-w-container mx-auto lgl:px-20 pt-24 mdl:pb-24">
       <SectionTitle title="Some things i have build" titleNo="03" />
-      <div className="flex flex-col w-full items-center justify-center gap-28 mt-20">
-        {/* project 1 */}
-        <div className="flex flex-col xl:flex-row gap-6">
-        
-            <a href="#" className="w-[500px] xl:w-1/2 h-auto relative group">
-              <div>
-                <Image
-                  className="w-full h-[500px] object-contain"
-                  src={images}
-                  alt="project-image"
-                />
-              </div>
-            </a>
-        
-          <div className="w-full xl:w-1/2 flex flex-col gap-6 items-end lgl:justify-evenly text-right xl:-ml-16 z-10">
-            <p className="font-titleFont text-textGreen  text-sm tracking-wide">
-              Feature Project
-            </p>
-            <h3 className="text-2xl font-bold">Amazon Bold</h3>
-            <p className="bg-[#112240] text-sm md:text-base p-2 md:p-6 rounded-md">
-              This is a simple view of website that take to create a new model
-              in our the maintain abusive word all time avoid . a new model in
-              our the maintain abusive word all time avoid
-            </p>
-            <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
-              <li>Typescript</li>
-              <li>React Js</li>
-              <li>MongoDB</li>
-              <li>Express</li>
-              <li>Vercel Deployment</li>
-            </ul>
-            <div className="text-2xl flex gap-2">
-              <a
-                href="#"
-                className="hover:text-textGreen duration-500 w-10 h-10 rounded-full bg-hoverColor hover:-translate-y-2 z-20 flex items-center justify-center "
-              >
-                <TbBrandGithub />
-              </a>
-              <a
-                href="#"
-                className="hover:text-textGreen duration-500 w-10 h-10 rounded-full bg-hoverColor hover:-translate-y-2 z-20 flex items-center justify-center "
-              >
-                <TbBrandGithub />
-              </a>
-              <a
-                href="#"
-                className="hover:text-textGreen duration-500 w-10 h-10 rounded-full bg-hoverColor hover:-translate-y-2 z-20 flex items-center justify-center "
-              >
-                <TbBrandGithub />
-              </a>
+    <div className="grid grid-cols-1 mdl:grid-cols-2 lgl:grid-cols-3 gap-10 my-16">
+        {/* PROJECT 1 */}
+        <div className="shadow-cardShadow px-3 py-5 rounded-xl">
+        <div className="w-full flex flex-col justify-center">
+        <div className='content'>
+       <div className='screen '>
+                <Image className='w-full h-[100px]'  src={eventMart} alt="event-mart" />
             </div>
+       </div>
+        </div>
+        <div className="my-2">
+          <h2 className="text-textGreen text-xl">Event Mart</h2>
+          <h4 className="text-textLight my-2 text-[22px] mdl:text-[30px]">Event Booking Website</h4>
+          <div className="flex justify-between shadow-cardShadow px-4 py-2 rounded-2xl">
+            <button className="flex items-center gap-1 shadow-cardShadow px-2 py-2 rounded-lg hover:bg-hoverColor transition-all duration-400"><FaCircleInfo/><span>View Details</span></button>
+            <a href="https://event-app-pi.vercel.app/" target="_blank">
+            <button className="flex items-center gap-1 shadow-cardShadow px-2 py-2 rounded-lg hover:bg-hoverColor transition-all duration-400"><MdOutlineLiveTv/><span>Live Site</span></button></a>
           </div>
         </div>
-        {/* project 2 */}
-        <div className="flex flex-col xl:flex-row-reverse gap-6">
-       
-            <a href="#" className="w-[500px] xl:w-1/2 h-auto relative group">
-              <div>
-                <Image
-                  className="w-full h-[500px] object-contain"
-                  src={images}
-                  alt="project-image"
-                />
-              </div>
-            </a>
-          
-          <div className="w-full xl:w-1/2 flex flex-col gap-6 items-end lgl:justify-evenly text-left z-10">
-            <p className="font-titleFont  text-textGreen  text-sm tracking-wide">
-              Feature Project
-            </p>
-            <h3 className="text-2xl font-bold">Amazon Bold</h3>
-            <p className="bg-[#112240] text-sm md:text-base p-2 md:p-6 rounded-md  xl:-mr-16">
-              This is a simple view of website that take to create a new model
-              in our the maintain abusive word all time avoid . a new model in
-              our the maintain abusive word all time avoid
-            </p>
-            <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-textDark">
-              <li>Typescript</li>
-              <li>React Js</li>
-              <li>MongoDB</li>
-              <li>Express</li>
-              <li>Vercel Deployment</li>
-            </ul>
-            <div className="text-2xl flex gap-2">
-              <a
-                href="#"
-                className="hover:text-textGreen duration-500 w-10 h-10 rounded-full bg-hoverColor hover:-translate-y-2 z-20 flex items-center justify-center "
-              >
-                <TbBrandGithub />
-              </a>
-              <a
-                href="#"
-                className="hover:text-textGreen duration-500 w-10 h-10 rounded-full bg-hoverColor hover:-translate-y-2 z-20 flex items-center justify-center "
-              >
-                <TbBrandGithub />
-              </a>
-              <a
-                href="#"
-                className="hover:text-textGreen duration-500 w-10 h-10 rounded-full bg-hoverColor hover:-translate-y-2 z-20 flex items-center justify-center "
-              >
-                <TbBrandGithub />
-              </a>
+        </div>
+        
+        {/* PROJECT 2 */}
+        <div className="shadow-cardShadow px-3 py-5 rounded-xl">
+        <div className="w-full flex flex-col justify-center">
+        <div className='content'>
+       <div className='screen-project-1 '>
+                <Image className='w-full h-[100px]'  src={laptop} alt="event-mart" />
             </div>
+       </div>
+        </div>
+        <div className="my-2">
+          <h2 className="text-textGreen text-xl">Resale Laptop</h2>
+          <h4 className="text-textLight my-2 text-[22px] mdl:text-[30px]">Reselling Laptop Website</h4>
+          <div className="flex justify-between shadow-cardShadow px-4 py-2 rounded-2xl">
+            <button className="flex items-center gap-1 shadow-cardShadow px-2 py-2 rounded-lg hover:bg-hoverColor transition-all duration-400"><FaCircleInfo/><span>View Details</span></button>
+            <a href="https://resell-product.web.app/" target="_blank">
+            <button className="flex items-center gap-1 shadow-cardShadow px-2 py-2 rounded-lg hover:bg-hoverColor transition-all duration-400"><MdOutlineLiveTv/><span>Live Site</span></button></a>
           </div>
         </div>
-      </div>
+        </div>
+        
+        {/* PROJECT 3 */}
+        <div className="shadow-cardShadow px-3 py-5 rounded-xl">
+        <div className="w-full flex flex-col justify-center">
+        <div className='content'>
+       <div className='screen-project-3 '>
+                <Image className='w-full h-[100px]'  src={onlineService} alt="event-mart" />
+            </div>
+       </div>
+        </div>
+        <div className="my-2">
+          <h2 className="text-textGreen text-xl">Event Mart</h2>
+          <h4 className="text-textLight my-2 text-[22px] mdl:text-[30px]">Event Booking Website</h4>
+          <div className="flex justify-between shadow-cardShadow px-4 py-2 rounded-2xl">
+            <button className="flex items-center gap-1 shadow-cardShadow px-2 py-2 rounded-lg hover:bg-hoverColor transition-all duration-400"><FaCircleInfo/><span>View Details</span></button>
+            <a href="https://genius-cart-6d01c.web.app/" target="_blank">
+            <button className="flex items-center gap-1 shadow-cardShadow px-2 py-2 rounded-lg hover:bg-hoverColor transition-all duration-400"><MdOutlineLiveTv/><span>Live Site</span></button></a>
+          </div>
+        </div>
+        </div>
+        
+        </div>
     </section>
   );
 };
