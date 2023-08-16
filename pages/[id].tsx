2de80381d45ca080ad4e3c6bee6cc5dfdd68b2af
mdansarulislam641 <mdansarulislam641.com>
2023-08-16 08:28:52 +0600

    project details part done

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -4,6 +4,7 @@ import { GetServerSideProps } from "next";
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import {BsFillSendFill ,BsGithub } from 'react-icons/bs'
+import { PhotoProvider, PhotoView } from "react-photo-view";
 interface ProjectDetailProps {
   project: Project;
 }
@@ -22,8 +23,10 @@ const ProjectDetailPage: React.FC<ProjectDetailProps> = ({ project }) => {
   const width = "1000";
   const height = "100";
   return (
-    <section className="w-full h-screen bg-bodyColor text-textLight font-bodyFont overflow-x-hidden overflow-y-scroll">
+    <section className="w-full h-screen bg-bodyColor text-textLight font-bodyFont overflow-x-hidden overflow-y-scroll pb-20">
+      <div className="sticky top-0 left-0 z-50">
       <Navbar />
+      </div>
       <div className="max-w-contentContainer my-24 mx-auto flex flex-col lg:flex-row shadow-cardShadow px-5 py-5 rounded-xl gap-5 justify-around group relative">
         {/* img  */}
         <div className="w-full flex flex-col justify-center flex-1">
@@ -72,6 +75,22 @@ const ProjectDetailPage: React.FC<ProjectDetailProps> = ({ project }) => {
              
               </div>
       </div>
+      {/* screenShorts */}
+    
+      <div className='max-w-contentContainer mx-auto'>
+      <h4 className="text-3xl mdl:text-4xl lgl:text-5xl text-center text-textGreen py-20">Projects Images</h4>
+                {/* card  */}
+                <div className='grid grid-cols-1 lgl:grid-cols-2 gap-10 px-5'>
+                <PhotoProvider>
+                   { screenShortImage.map(( img , idx) => (
+                      <PhotoView key={idx}  src={img}> 
+                            <Image className='w-full md:w-2/3 mx-auto lgl:w-full h-[500px] object-fill cursor-pointer rounded-lg shadow-cardShadow'  title='click to view' src={img} width={1000} height={500} alt='achieve-image' />
+                      </PhotoView>
+                   ))}
+                 
+                    </PhotoProvider>
+                </div>
+            </div>
     </section>
   );
 };
